129ccad9bec638829440248a621d99c490a49a23
Klurifixus <144474109+Klurifixus@users.noreply.github.com>
2023-11-27 21:10:14 +0100

    #3 add svg fuction for darkmode

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 function showReview(){
-    //document.querySelector('.review-con').style.opacity = '100';
-    //document.querySelector('.product-img').style.filter = 'brightness(0.4)';
     document.querySelector('.review-con').classList.toggle('hover-review');
     document.querySelector('.product-img').classList.toggle("hover-img");
 }
 function hideReview(){
-    //document.querySelector('.review-con').style.opacity = '0';
-    //document.querySelector('.product-img').style.filter = 'brightness(1)';
     document.querySelector('.review-con').classList.toggle('hover-review');
     document.querySelector('.product-img').classList.toggle("hover-img");
+}
+
+var sun = document.getElementById('sun-icon')
+var moon = document.getElementById('moon-icon')
+var body = document.getElementsByTagName('body')[0]
+var introText = document.querySelector('#intro p')
+
+function darkMode(){
+    sun.style.display="none";
+    moon.style.display="block";
+    body.classList.toggle('body-dark');
+    introText.style.color="#FFFFFF";
+
+}
+function lightMode(){
+    sun.style.display="block";
+    moon.style.display="none";
+    body.classList.toggle('body-dark');
+    introText.style.color="#FFFFFF";
+
 }
